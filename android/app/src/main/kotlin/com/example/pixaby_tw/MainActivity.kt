05cc49cfc3c0d8616cf2c5bf5bc060d75b1d39e2
Rithik Bhandari <56810766+rithik-dev@users.noreply.github.com>
2024-04-16 21:27:54 +0530

    feat: added build to vcs for deployment
    (cherry picked from commit bd5a96682db8b1c14df5670f920829f5add9d5d6)

--- android/app/src/main/kotlin/com/example/pixaby_tw/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pixaby_tw
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/base_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
-
-mixin BaseRepository {
-  @protected
-  Dio get dio => _dio;
-
-  static final _dio = Dio();
-}
